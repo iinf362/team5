5d1a70ffc3d84ad1a217d343a04a3047d6327414
Jacob Messina <JMessina@albany.edu>
2014-12-01 20:49:39 -0500

    still more edits

--- js/main.js
+++ js/main.js
@@ -15,6 +15,25 @@ $("#panel3").click(function(){
   $("#panelC3").toggle(1000);
 });
 
+//panel4
+$("#panel4").click(function(){
+  $("#panelC4").toggle(1000);
+});
+//panel5
+$("#panel5").click(function(){
+  $("#panelC5").toggle(1000);
+});
+//panel6
+$("#panel6").click(function(){
+  $("#panelC6").toggle(1000);
+});
+//panel7
+$("#panel7").click(function(){
+  $("#panelC7").toggle(1000);
+});
+
 $(document).ready(function(){
     $(".nav-internal").sticky({topSpacing:5});
   });
+  
+  
